ba892e3475c22dd9b775b485fa7c9ac47f4a2740
Steven Chen <ejsteven@gmail.com>
2020-09-13 13:01:45 +1000

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import socketIOClient from 'socket.io-client';
+const ENDPOINT = 'http://127.0.0.1:4001';
+
+const socket = socketIOClient(ENDPOINT);
 
 function App() {
+  const [display, setDisplay] = useState('');
+  const [input, setInput] = useState('');
+
+  useEffect(() => {
+    socket.on('latestMessage', data => {
+      setDisplay(data);
+    });
+    return () => socket.disconnect();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <input value={input} onInput={e => setInput(e.target.value)} />
+        <button onClick={() => {
+          console.log(input)
+          socket.emit('newMessage', input)
+          setInput('')
+        }}>Send</button>
+      </div>
+      <div>
+        <span>Latest message: </span><span>{display}</span>
+      </div>
+    </>
   );
 }
 
